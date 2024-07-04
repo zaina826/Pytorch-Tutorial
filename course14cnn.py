cfb98b05e6e9e65aae58d41aabe1ddeec6c4e3f1
zaina826 <zainaschaban@gmail.com>
2024-07-05 02:48:50 +0300

    course 14 the rest of it, no data

--- course14cnn.py
+++ course14cnn.py
@@ -17,7 +17,7 @@ learning_rate=0.001
 #dataset has PILImage of range [0,1] but we normalize to get range of [-1,1] using a transform
 transform= transforms.Compose(
     #A composed transform first to tensor, then normalize
-    [transforms.ToTensor,
+    [transforms.ToTensor(),
      transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))]
      #This sets 0.5 as the mean, and 0.5 as also the standard deviation
      #so zero will become -1, 1 will become 1 and all other values
@@ -36,12 +36,39 @@ classes=('plane', 'car', 'bird', 'cat','deer','dog','frog','horse','ship','truck
 
 class CNN(nn.Module):
     def __init__(self):
-        pass
-    def forward(self):
-        pass
+        super(CNN, self).__init__()
+        #(input channel size, output channel size)
+        #initially the shape is 4, 3, 32, 32
+        self.conv1 = nn.Conv2d(3,6,5) 
+        #After the first convultion since we don't have padding, it will become 
+        #4,6,28,28
+        #6 Output channels (each one of them done by a certain kernel)
+        #Each kernel is of size 5  
+        #We don't actually decide what the kernel does or what it looks like
+        #These are parameters just like weights and biases that change through out the training.
+
+        self.pool= nn.MaxPool2d(2,2) #Kernel size of 2, and a stride of
+        #Now pooling by a kernel of size 2 will divide the pixels in half, so 4,6,14,14
+        self.conv2= nn.Conv2d(6,16,5)
+        #4,16,10,10
+        #Now if we apply pooling again: 4,16,5,5
+        #so this is why the first linear layer must have an input of 16*5*5
+        self.fc1=nn.Linear(16*5*5, 120)
+        self.fc2=nn.Linear(120, 84)
+        self.fc3=nn.Linear(84, 10)
+
+    def forward(self,x):
+        x=self.pool(F.relu(self.conv1(x))) #First conv then pooling
+        x=self.pool(F.relu(self.conv2(x))) #Second
+        #This is called Flattening, so we can plug it into a linear function
+        x=x.view(-1,16*5*5)
+        x=F.relu(self.fc1(x))
+        x=F.relu(self.fc2(x))
+        x=self.fc3(x) #No activation function, cuz softmax is included in the loss
+        return x
 
 model= CNN().to(device)
-criterion=nn.CrossEntropyLoss()
+criterion=nn.CrossEntropyLoss() #Remember softmax is already included here
 optimizer= torch.optim.SGD(model.parameters(),lr=learning_rate)
 
 n_total_steps=len(train_loader)
